b402698d2c954815e43279987922019875e959e7
kevin31esder <kevin31esder@gmail.com>
2022-06-04 19:21:16 -0500

    make it better

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      "mongodb+srv://admin:LcHOphUADbmp8ZwS@cluster0.6vjhqry.mongodb.net/?retryWrites=true&w=majority",
-      {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-      }
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.error(`Error: ${error.message}`.underline.bold);
